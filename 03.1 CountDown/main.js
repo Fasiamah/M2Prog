48138f1e3438128553c4096eecdf4239b38ec4fe
Frankini <frankasiamah2008@gmail.com>
2025-12-02 10:00:41 +0100

    aster

---
+++ 03.1 CountDown/main.js
@@ -0,0 +1,24 @@
+let now = new Date()
+
+
+let release = new Date("19 november 2026");
+
+let daysElement = document.getElementById("days");
+let hoursElement = document.getElementById("hours");
+let minutesElement = document.getElementById("minutes");
+let secondsElement = document.getElementById("seconds");
+
+
+
+
+let miliseconds = release - now;
+let days = Math.floor(miliseconds / (1000 * 60 * 60 * 24));
+let hours = Math.floor((miliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutes = Math.floor((miliseconds % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((miliseconds % (1000 * 60)) / 1000);
+console.log(days, hours, minutes, seconds);
+
+daysElement.innerText = days + " Days";
+hoursElement.innerText = hours + " Hours";
+minutesElement.innerText = minutes + " Minutes";
+secondsElement.innerText = seconds + " Seconds";
