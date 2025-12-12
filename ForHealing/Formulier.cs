84568347c911c750f7b20394b3f12ca464a2d325
Frankini <frankasiamah2008@gmail.com>
2025-12-12 15:15:10 +0100

    ain

---
+++ ForHealing/Formulier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForHealing
+{
+    internal class Formulier
+    {
+        internal int Sterren { get; set; }
+        internal string Feedback { get; set; }
+    }
+}
---
+++ ForHealing/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForHealing
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            double[] prijzen = new double[] { 0.99, 5.60, 10.11, 4.20}; //als je 3 prijzen hebt en 4 artiekelen, dan werkt het niet, omdat de artiekel en prijs niet samen zijn
+            string[] artiekelen = { "snoepje", "luxe broodje", "lunch menu", "maaltijd" };
+            Formulier[] formulieren = new Formulier[2];
+
+            formulieren[0] = new Formulier()
+            {
+                Feedback = "prijzig maar lekker",
+                Sterren = 5,
+            };
+            for (int i = 0; i < prijzen.Length; i++)
+            {
+                Console.WriteLine(prijzen[i]);
+                Console.WriteLine(artiekelen[i]);
+            }
+        }
+    }
+}
