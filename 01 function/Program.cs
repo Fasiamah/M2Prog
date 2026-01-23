689449f1c6a38a8f4418920d7ff1b504d697c010
Frankini <frankasiamah2008@gmail.com>
2026-01-23 14:40:23 +0100

    new

---
+++ 01 function/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01_function;
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program(); //een string variabelen genaamd program samen met de variabelen defenition program
+            program.run();//gebruiken van de function run
+        }
+    }  
+}
